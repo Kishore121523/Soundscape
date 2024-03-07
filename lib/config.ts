6866ef42a0624bdc6c6275a28fd74726d969723a
rogue-gamer-ryt <ashun.kothari10@gmail.com>
2024-03-07 15:12:42 -0500

    keys removed

--- lib/config.ts
+++ lib/config.ts
@@ -4,5 +4,5 @@ export const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/api";
 export const SPOTIFY_API_BASE_URL = 'https://api.spotify.com/v1';
 
 // TODO: Move this to .env
-export const SPOTIFY_CLIENT_ID = 'c9e199990058485b94cd1f7d354b8d9e';
-export const SPOTIFY_CLIENT_SECRET = '5111af08a4de4b1a8239b3ecc7f23883';
+export const SPOTIFY_CLIENT_ID = '';
+export const SPOTIFY_CLIENT_SECRET = '';
